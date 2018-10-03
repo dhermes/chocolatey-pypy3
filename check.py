8fa7640f777891747f5f1e2dbc103f59d68cf1ba
Danny Hermes <daniel.j.hermes@gmail.com>
2018-10-03 14:07:45 -0700

    Making `check.py` a little more robust.
    Also saving the `nupkg` as an artifact so we can avoid building
    it on future runs.

--- check.py
+++ check.py
@@ -5,14 +5,30 @@ import os
 import sys
 
 
+SEPARATOR = "=" * 60
+
+
+def all3(name, value):
+    print(SEPARATOR)
+    print("{}:".format(name))
+    print(type(value))
+    print(value)
+    print(repr(value))
+
+
 def main():
     # size = os.get_terminal_size(sys.__stdout__.fileno())
     stdout = sys.__stdout__
-    print(stdout)
+    all3("stdout", stdout)
+
     fileno = stdout.fileno()
-    print(fileno)
-    size = os.get_terminal_size(fileno)
-    print(size)
+    all3("fileno", fileno)
+
+    try:
+        size = os.get_terminal_size(fileno)
+        all3("size", size)
+    except Exception as exc:
+        all3("exc", exc)
 
 
 if __name__ == "__main__":
